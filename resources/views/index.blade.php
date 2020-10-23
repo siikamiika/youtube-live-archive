830c52b3e6b7aa72f0fd372ed33953eeee65c70d
siikamiika <siikamiika@users.noreply.github.com>
2020-10-23 04:56:40 +0300

    channel listing with images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,16 +6,22 @@
         <title>YouTube Live Archive</title>
 
         <link href="/css/style.css" rel="stylesheet">
+        <link href="/css/index.style.css" rel="stylesheet">
     </head>
     <body>
         <div>
             <a href="{{ rroute('video_archive') }}">Archive new video or channel</a>
         </div>
         <h1>Channels</h1>
-        <ul>
+        <ul class="channel-list">
             @foreach ($channels as $channel)
-                <li>
-                    <a href="{{ rroute('channel', ['channel' => $channel]) }}">{{ $channel->name }}</a>
+                <li class="channel">
+                    <a href="{{ rroute('channel', ['channel' => $channel]) }}">
+                        <div class="channel-avatar"><img src="/storage/images/channel_avatars/{{ $channel->id }}/avatar.jpeg"></div>
+                        <div class="channel-text">
+                            <div class="channel-name">{{ $channel->name }}</div>
+                        </div>
+                    </a>
                 </li>
             @endforeach
         </ul>
