2792742c1df8233f8b00d548ab7cd6da50e22f42
siikamiika <siikamiika@users.noreply.github.com>
2021-07-01 03:09:31 +0300

    locale workaround for escapeshellarg

--- app/Domain/SystemCommand.php
+++ app/Domain/SystemCommand.php
@@ -6,6 +6,12 @@ class SystemCommand
 {
     public static function run(string $command, array $args)
     {
-        return shell_exec($command . ' ' . implode(' ', array_map('escapeshellarg', $args)));
+        // what he said
+        // https://github.com/mpv-player/mpv/commit/1e70e82baa9193f6f027338b0fab0f5078971fbe
+        $oldLocale = setlocale(LC_CTYPE, 0);
+        setlocale(LC_CTYPE, 'en_US.UTF_8');
+        $output = shell_exec($command . ' ' . implode(' ', array_map('escapeshellarg', $args)));
+        setlocale(LC_CTYPE, $oldLocale);
+        return $output;
     }
 }
